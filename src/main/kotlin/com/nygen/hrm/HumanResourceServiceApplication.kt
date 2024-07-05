c391ba15b281d90f8a717fdc1b152ec3fb92c81c
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-07-05 17:14:53 +0530

    Initial commit

---
+++ src/main/kotlin/com/nygen/hrm/HumanResourceServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.nygen.hrm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HumanResourceServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<HumanResourceServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/nygen/hrm/HumanResourceServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nygen.hrm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HumanResourceServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
