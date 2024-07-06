a10812043fa10b2461c4dbc726da1c0a41ef942e
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-07-07 01:02:16 +0530

    Implemented services to inserting and retrieving records from the database

--- src/main/kotlin/com/nygen/hrm/controller/MessageController.kt
+++ src/main/kotlin/com/nygen/hrm/controller/MessageController.kt
@@ -1,13 +1,17 @@
 package com.nygen.hrm.controller
 
 import com.nygen.hrm.domain.Message
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import com.nygen.hrm.service.MessageService
+import org.springframework.web.bind.annotation.*
 
 @RestController
-class MessageController {
+class MessageController(val service:MessageService) {
 
     @GetMapping("/")
-    fun getIndex(@RequestParam("name") name: String)= listOf(Message("1","First Object"),Message("2","Second Object"),Message("3","Third Object"),)
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping("/")
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
 }
--- src/main/kotlin/com/nygen/hrm/service/MessageService.kt
+++ src/main/kotlin/com/nygen/hrm/service/MessageService.kt
@@ -1,2 +1,21 @@
 package com.nygen.hrm.service
 
+import com.nygen.hrm.domain.Message
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Service
+import java.util.UUID
+
+@Service
+class MessageService(val db:JdbcTemplate) {
+    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun save(message: Message) {
+        val id = message.id?:UUID.randomUUID().toString()
+        db.update(
+            "insert into messages values ( ?, ? )",
+            id, message.text
+        )
+    }
+}
