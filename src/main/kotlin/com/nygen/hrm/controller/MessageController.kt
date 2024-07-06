c0bec06e6887be4039bb00aa77a9c89ad281b438
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-07-06 23:44:44 +0530

    Adding a data class to the spring boot application

--- src/main/kotlin/com/nygen/hrm/controller/MessageController.kt
+++ src/main/kotlin/com/nygen/hrm/controller/MessageController.kt
@@ -1,5 +1,6 @@
 package com.nygen.hrm.controller
 
+import com.nygen.hrm.domain.Message
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -8,5 +9,5 @@ import org.springframework.web.bind.annotation.RestController
 class MessageController {
 
     @GetMapping("/")
-    fun getIndex(@RequestParam("name") name: String)="Hello, $name"
+    fun getIndex(@RequestParam("name") name: String)= listOf(Message("1","First Object"),Message("2","Second Object"),Message("3","Third Object"),)
 }
---
+++ src/main/kotlin/com/nygen/hrm/domain/Message.kt
@@ -0,0 +1,3 @@
+package com.nygen.hrm.domain
+
+data class Message(val id:String?, val text:String)
