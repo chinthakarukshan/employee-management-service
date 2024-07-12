5fe74d5f76d39256aa4ea62dcd308e9bb72fdf7b
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-07-13 01:14:32 +0530

    Changed the project to spring webflux project

--- src/main/kotlin/com/nygen/hrm/repository/MessageRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.nygen.hrm.repository
-
-import com.nygen.hrm.domain.Message
-import org.springframework.data.repository.CrudRepository
-
-interface MessageRepository:CrudRepository<Message,String>
